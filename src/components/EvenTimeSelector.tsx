ef1775e523e81c6b2874abb3332378454c046edd
serkanisyapan <serkanisyapan1@hotmail.com>
2024-03-24 16:45:31 +0300

    add event time select

---
+++ src/components/EvenTimeSelector.tsx
@@ -0,0 +1,96 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const twelveHourClock = [
+  "12 am",
+  "1 am",
+  "2 am",
+  "3 am",
+  "4 am",
+  "5 am",
+  "6 am",
+  "7 am",
+  "8 am",
+  "9 am",
+  "10 am",
+  "11 am",
+  "12 pm",
+  "1 pm",
+  "2 pm",
+  "3 pm",
+  "4 pm",
+  "5 pm",
+  "6 pm",
+  "7 pm",
+  "8 pm",
+  "9 pm",
+  "10 pm",
+  "11 pm",
+];
+const twentyFourHoursArray = [
+  "00",
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+];
+function createMinsArray() {
+  let minsArray = [];
+  for (let i = 0; i < 60; i++) {
+    if (i < 10) minsArray.push(`0${i}`);
+    if (i >= 10) minsArray.push(`${i}`);
+  }
+  return minsArray;
+}
+
+export function EventTimeSelector() {
+  const minsArray = createMinsArray();
+  return (
+    <div className="flex flex-row gap-2">
+      <Select>
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder="hrs" />
+        </SelectTrigger>
+        <SelectContent>
+          {twelveHourClock.map((clock) => (
+            <SelectItem value={`${clock}`}>{clock}</SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Select>
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder="mins" />
+        </SelectTrigger>
+        <SelectContent>
+          {minsArray.map((mins) => (
+            <SelectItem value={`${mins}`}>{mins} min</SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 import { DatePicker } from "./DatePicker";
+import { EventTimeSelector } from "./EvenTimeSelector";
 
 export function Timer() {
   const [timerName, setTimerName] = useState("Timer Event Name");
@@ -10,16 +12,26 @@ export function Timer() {
   }
 
   return (
-    <div>
-      <Input
-        type="text"
-        placeholder="Event Name"
-        onChange={(event) => setTimerName(event.target.value)}
-        onFocus={() => resetTimerNameOnFocus()}
-        value={timerName}
-      ></Input>
-      <h1 className="text-white text-xl">{timerName}</h1>
-      <DatePicker />
+    <div className="max-w-s flex flex-col gap-5 px-10 mt-7">
+      <div className="grid w-full max-w-sm items-center gap-2">
+        <Label htmlFor="event_name">Event Name</Label>
+        <Input
+          type="text"
+          id="event_name"
+          placeholder="Event Name"
+          onChange={(event) => setTimerName(event.target.value)}
+          onFocus={() => resetTimerNameOnFocus()}
+          value={timerName}
+        />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-2">
+        <Label htmlFor="date_picker">Event Date</Label>
+        <DatePicker />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-2">
+        <Label htmlFor="event_time">Event Time</Label>
+        <EventTimeSelector />
+      </div>
     </div>
   );
 }
